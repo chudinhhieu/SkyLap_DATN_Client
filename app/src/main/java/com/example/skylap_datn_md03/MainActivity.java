5882c544745984467343fd654c0ede0a2663c9fb
python3112 <hoangporon3112@gmail.com>
2024-01-29 14:53:57 +0700

    test push code Hoàng

--- app/src/main/java/com/example/skylap_datn_md03/MainActivity.java
+++ app/src/main/java/com/example/skylap_datn_md03/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.skylap_datn_md03;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView txt_test;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        init_ui();
     }
+    void init_ui(){
+        txt_test = findViewById(R.id.txt_test);
+    }
+
 }
