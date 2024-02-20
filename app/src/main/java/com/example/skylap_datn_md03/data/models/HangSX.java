f18c8dd3a4fca80c54897a897c9de7a09e690f14
ca van chung <cachung9a4@gmail.com>
2024-02-20 15:07:06 +0700

    cachung task2

--- app/src/main/java/com/example/skylap_datn_md03/data/models/HangSX.java
+++ app/src/main/java/com/example/skylap_datn_md03/data/models/HangSX.java
@@ -3,13 +3,17 @@ package com.example.skylap_datn_md03.data.models;
 public class HangSX {
     private String _id;
     private String tenHangSx;
+    private String logo;
     private boolean trangThai;
 
     public HangSX() {
     }
 
-    public HangSX(String tenHangSx, boolean trangThai) {
+
+    public HangSX(String _id, String tenHangSx, String logo, boolean trangThai) {
+        this._id = _id;
         this.tenHangSx = tenHangSx;
+        this.logo = logo;
         this.trangThai = trangThai;
     }
 
@@ -35,5 +39,13 @@ public class HangSX {
 
     public void setTrangThai(boolean trangThai) {
         this.trangThai = trangThai;
+    }
+
+    public String getLogo() {
+        return logo;
+    }
+
+    public void setLogo(String logo) {
+        this.logo = logo;
     }
 }
