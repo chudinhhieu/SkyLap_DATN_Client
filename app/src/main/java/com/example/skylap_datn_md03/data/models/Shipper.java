3d1dfe9d96dc5a206338360cf343f2aa89583ebe
hoangt <78737006+python3112@users.noreply.github.com>
2024-03-19 16:27:43 +0700

    Delete app directory

--- app/src/main/java/com/example/skylap_datn_md03/data/models/Shipper.java
+++
@@ -1,8 +0,0 @@
-package com.example.skylap_datn_md03.data.models;
-
-public class Shipper {
-    private String _id;
-    private String hoTen;
-    private String sdt;
-    private String avatar;
-}
