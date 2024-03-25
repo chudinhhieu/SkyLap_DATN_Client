8388fe986b3019bbd10b29acfe568ed8efc856bc
vanh19 <anhmv19@gmail.com>
2024-03-25 20:32:22 +0700

    mh khuyenMai

---
+++ app/src/main/java/com/example/skylap_datn_md03/adapter/KhuyenMaiAdapter.java
@@ -0,0 +1,68 @@
+package com.example.skylap_datn_md03.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.skylap_datn_md03.R;
+import com.example.skylap_datn_md03.data.models.KhuyenMai;
+import com.squareup.picasso.Picasso;
+
+import java.text.SimpleDateFormat;
+import java.util.List;
+import java.util.Locale;
+
+public class KhuyenMaiAdapter extends RecyclerView.Adapter<KhuyenMaiAdapter.KhuyenMaiViewHolder>{
+    private List<KhuyenMai> list;
+    private Context context;
+
+    public KhuyenMaiAdapter(List<KhuyenMai> list, Context context){
+        this.list = list;
+        this.context = context;
+    }
+
+
+    @NonNull
+    @Override
+    public KhuyenMaiAdapter.KhuyenMaiViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_giamgia, parent, false);
+        return new KhuyenMaiViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull KhuyenMaiViewHolder holder, int position) {
+        KhuyenMai khuyenMai = list.get(position);
+        holder.item_giamgia_mota.setText(khuyenMai.getMoTa());
+        holder.item_giamgia_code.setText(khuyenMai.getCode());
+
+        Picasso.get().load(khuyenMai.getAnh()).into(holder.item_giamgia_image);
+
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+        holder.item_giamgia_thoigian.setText(dateFormat.format(khuyenMai.getThoiGianKetThuc()));
+    }
+
+    @Override
+    public int getItemCount() {
+        if (list != null) return list.size();
+        return 0;
+    }
+
+    public class KhuyenMaiViewHolder extends RecyclerView.ViewHolder {
+        private ImageView item_giamgia_image;
+        private TextView item_giamgia_mota, item_giamgia_code, item_giamgia_thoigian;
+        public KhuyenMaiViewHolder(@NonNull View itemView) {
+            super(itemView);
+            item_giamgia_image = itemView.findViewById(R.id.item_giamgia_image);
+            item_giamgia_mota = itemView.findViewById(R.id.item_giamgia_mota);
+            item_giamgia_code = itemView.findViewById(R.id.item_giamgia_code);
+            item_giamgia_thoigian = itemView.findViewById(R.id.item_giamgia_thoigian);
+        }
+    }
+}
